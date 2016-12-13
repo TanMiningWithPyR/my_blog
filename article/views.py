5804ee8c400a52abb7b3c0294a1d3a9e55e992db
tanfuyou <fuyou_tan@sina.cn>
2016-12-13 16:06:31 +0800

    django tutorial init

---
+++ article/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+
+def detail(request, my_args):
+    return HttpResponse("You're looking at my_args %s." % my_args)
+
+def detail_database(request, my_args):
+    post = Article.objects.all()[int(my_args)]
+    str = ("title = %s, category = %s, data_time = %s, content = %s"
+           % (post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
+
+def home(request):
+    post_list = Article.objects.all()   # to get all Article objects
+    return render(request, 'home.html', {'post_list': post_list})
